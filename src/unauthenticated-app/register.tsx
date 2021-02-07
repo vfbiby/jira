27bd49d570f0db046019d73eb01fcdf15ace423a
James Zhang <liulfbaby@gmail.com>
2021-02-07 16:35:07 +0800

    feat: confirm password on register

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -10,10 +10,18 @@ export const RegisterScreen = ({
 }) => {
   const { register } = useAuth();
 
-  const handleSubmit = async (values: {
+  const handleSubmit = async ({
+    cpassword,
+    ...values
+  }: {
     username: string;
     password: string;
+    cpassword: string;
   }) => {
+    if (cpassword !== values.password) {
+      onError(new Error('请确认两次输入的密码相同'));
+      return;
+    }
     try {
       await register(values);
     } catch (error) {
@@ -34,6 +42,13 @@ export const RegisterScreen = ({
         label="Password"
         name="password"
         rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="cpassword"
+        rules={[{ required: true, message: 'Please confirm your password!' }]}
       >
         <Input />
       </Form.Item>
